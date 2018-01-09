0e223cf2fc95644ac0e40cb13417f13c6f29ee6e
yelo <zhihuzeye@gmail.com>
2018-01-09 23:16:47 +0800

    draft

--- packages/mina-loader/lib/helpers.js
+++ packages/mina-loader/lib/helpers.js
@@ -1,3 +1,4 @@
+const vm = require('vm')
 const loaderUtils = require('loader-utils')
 
 exports.loadModule = function loadModule (request) {
@@ -55,4 +56,22 @@ exports.toSafeOutputPath = function (original) {
   return (original || '')
     .replace(/\.\./g, '_')
     .replace(/node_modules([\/\\])/g, '_node_modules_$1')
+}
+
+/**
+ * Forked from:
+ * https://github.com/peerigon/extract-loader/blob/f5a1946a7b54ef962e5af56aaf29d318efaabf66/src/extractLoader.js#L110
+ * https://github.com/Cap32/wxml-loader/blob/986c2a07f195c0f8f4e35169148e4965061a50f6/src/index.js#L21
+ */
+exports.extract = function (src, publicPath = '') {
+  const script = new vm.Script(src, {
+    displayErrors: true,
+  })
+  const sandbox = {
+    module: {},
+    __webpack_public_path__: publicPath,
+  }
+
+  script.runInNewContext(sandbox)
+  return sandbox.module.exports.toString()
 }
--- packages/mina-loader/lib/loaders/mina-json-file.js
+++ packages/mina-loader/lib/loaders/mina-json-file.js
@@ -1,8 +1,11 @@
 const path = require('path')
+const merge = require('lodash.merge')
 const compose = require('compose-function')
 const loaderUtils = require('loader-utils')
 const resolveFrom = require('resolve-from')
 const ensurePosix = require('ensure-posix-path')
+const pMap = require('p-map')
+const pAll = require('p-all')
 
 const helpers = require('../helpers')
 
@@ -28,20 +31,67 @@ function resolveFromModule (context, filename) {
 }
 
 module.exports = function (source) {
+  const done = this.async()
+
+  const options = merge({}, {
+    publicPath: this.options.output.publicPath,
+  }, loaderUtils.getOptions(this) || {})
+  const relativeToRoot = path.relative(path.dirname(this.resource), this.options.context)
+  const loadModule = helpers.loadModule.bind(this)
+
   let config = JSON.parse(source)
-  let relativeToRoot = path.relative(path.dirname(this.resource), this.options.context)
 
   if (!config) {
-    return ''
+    return done(null, '')
   }
 
-  if (Array.isArray(config.pages)) {
-    config.pages = config.pages.map((page) => resolveFile(this.context, page, this.options.context))
-  }
-
-  if (typeof config.usingComponents === 'object') {
-    config.usingComponents = mapObject(config.usingComponents, (file) => resolveFile(this.context, file, this.options.context))
-  }
-
-  return JSON.stringify(config, null, 2)
+  Promise.resolve(config)
+    /**
+     * pages
+     */
+    .then((config) => {
+      if (!Array.isArray(config.pages)) {
+        return config
+      }
+      return Object.assign(config, {
+        pages: config.pages.map((page) => resolveFile(this.context, page, this.options.context)),
+      })
+    })
+    /**
+     * usingComponent
+     */
+    .then((config) => {
+      if (typeof config.usingComponents !== 'object') {
+        return config
+      }
+      return Object.assign(config, {
+        usingComponents: mapObject(config.usingComponents, (file) => resolveFile(this.context, file, this.options.context)),
+      })
+    })
+    /**
+     * tabBar
+     */
+    .then((config) => {
+      if (!config.tabBar || !Array.isArray(config.tabBar.list)) {
+        return config
+      }
+      const extract = (source) => helpers.extract(source, options.publicPath)
+      return pMap(config.tabBar.list, (tab) => {
+        let files = []
+        if (tab.iconPath) {
+          files.push(() => loadModule(tab.iconPath).then(extract))
+        }
+        if (tab.selectedIconPath) {
+          files.push(() => loadModule(tab.selectedIconPath).then(extract))
+        }
+        return pAll(files)
+      })
+        .then((files) => {
+          console.log(files)
+          // TODO: flatten
+          return config
+        })
+    })
+    .then((config) => done(null, JSON.stringify(config, null, 2)))
+    .catch((error) => done(error))
 }
