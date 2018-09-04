42e476cc2169243118e285d5a4f7990b112911cc
Jiayu Liu <jiayu.liu@airbnb.com>
2018-09-04 23:44:59 +0800

    feat: add minimize option to minimize json

--- packages/mina-loader/lib/constants.js
+++ packages/mina-loader/lib/constants.js
@@ -11,11 +11,12 @@ const TYPES_FOR_FILE_LOADER = ['template', 'style', 'config']
 const TYPES_FOR_OUTPUT = ['script']
 
 const LOADERS = {
-  template: ({ publicPath, context }) =>
+  template: ({ publicPath, context, minimize }) =>
     `${resolve('@tinajs/wxml-loader')}?${JSON.stringify({
       publicPath,
       enforceRelativePath: true,
       root: context,
+      minimize,
       raw: true,
     })}`,
   style: ({ publicPath }) =>
@@ -23,8 +24,11 @@ const LOADERS = {
       'css-loader'
     )}`,
   script: () => '',
-  config: ({ publicPath }) =>
-    `${resolve('./loaders/mina-json-file')}?${JSON.stringify({ publicPath })}`,
+  config: ({ publicPath, minimize }) =>
+    `${resolve('./loaders/mina-json-file')}?${JSON.stringify({
+      publicPath,
+      minimize,
+    })}`,
 }
 
 exports.EXTNAMES = EXTNAMES
