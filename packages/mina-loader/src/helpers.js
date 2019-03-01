8f7272c9ee975234671b2105c5d11b7603ceb30c
Jiayu Liu <jiayu.liu@airbnb.com>
2019-03-01 22:22:03 +0800

    migrate to typescirpt

--- packages/mina-loader/src/helpers.js
+++ packages/mina-loader/src/helpers.js
@@ -59,11 +59,10 @@ exports.ensureBang = function ensureBang(loader) {
 
 exports.stringifyLoaders = function stringifyLoaders(loaders) {
   return loaders
-    .map(
-      obj =>
-        obj && typeof obj === 'object' && typeof obj.loader === 'string'
-          ? obj.loader + (obj.options ? '?' + JSON.stringify(obj.options) : '')
-          : obj
+    .map(obj =>
+      obj && typeof obj === 'object' && typeof obj.loader === 'string'
+        ? obj.loader + (obj.options ? '?' + JSON.stringify(obj.options) : '')
+        : obj
     )
     .join('!')
 }
